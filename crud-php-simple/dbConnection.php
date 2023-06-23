35f80d893b2e3ad48f393dccfca3b0fa0edbfe4b
Tashdid_Alam <130019268+TashdidAlam@users.noreply.github.com>
2023-06-23 10:26:06 +0600

    Add files via upload

---
+++ crud-php-simple/dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'db-cont';
+$databaseName = 'test';
+$databaseUsername = 'root';
+$databasePassword = 'bjit1234';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
